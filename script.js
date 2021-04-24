00564f9434bac7d667df2539fd873ec99af4dd1d
Esther V <esvalas@gmail.com>
2021-04-24 14:17:44 +0200

    add parallax effects

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-    .to('.rock', 3, {y: -600})
-    .to('.girl', 3, {y: -400}, '-=3')
-    .to('.bg', 3)
+    .to('.rock', 3, {y: -300})
+    .to('.girl', 3, {y: -200}, '-=10')
+    .fromTo('.bg', 3, {y: -50}, {y: 0, duration:10}, '-=10')
+    .to('.content', 3, {top:'0%'},'-=10')
+    .fromTo(".content-images", {opacity:0}, {opacity:1, duration:3})
+    .fromTo(".text", {opacity:0}, {opacity:1, duration:3});
+
+let scene = new ScrollMagic.Scene({
+    triggerElement: "section",
+    duration: "300%",
+    triggerHook: 0,
+})
+    .setTween(timeline)
+    .setPin("section")
+    .addTo(controller);
